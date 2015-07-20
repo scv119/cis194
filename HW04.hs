4d954650b6563ce3a8456201454e7404e18f657b
Chen Shen <chenshen@fb.com>
2015-07-19 23:29:24 -0700

    add

--- HW04.hs
+++ HW04.hs
@@ -15,12 +15,12 @@ x = P [0, 1]
 instance (Num a, Eq a) => Eq (Poly a) where
     P x == P y = x == y
     _ == _ = False
- 
+
 -- Exercise 3 -----------------------------------------
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
     show (P []) = "0"
-    show (P x) = intercalate " + " $ map showTerm $ filter (\(_, coeff) -> coeff /= 0) $ reverse $ zip [0..] x 
+    show (P x) = intercalate " + " $ map showTerm $ filter (\(_, coeff) -> coeff /= 0) $ reverse $ zip [0..] x
       where
         showTerm (degree, coeff)
           | degree == 0 = show coeff
@@ -37,15 +37,17 @@ plus (P x) (P y) = P $ zipWith (+) x y ++ drop minLen x ++ drop minLen y where
 -- Exercise 5 -----------------------------------------
 
 times :: Num a => Poly a -> Poly a -> Poly a
-times = undefined
+times (P x) (P y) = foldl plus (P []) result where
+  result = map timesY $ zip [0..] x where
+    timesY (degree, coeff) = P $ replicate degree 0 ++ map (*coeff) y
 
 -- Exercise 6 -----------------------------------------
 
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate      = undefined
-    fromInteger = undefined
+    negate (P x) = P $ map (* (-1)) x
+    fromInteger x = P [(fromInteger x)]
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
@@ -53,17 +55,18 @@ instance Num a => Num (Poly a) where
 -- Exercise 7 -----------------------------------------
 
 applyP :: Num a => Poly a -> a -> a
-applyP = undefined
+applyP (P x) y = foldl (\v (degree, coeff) -> v + y ^ degree * coeff) 0 $ zip [0..] x
 
 -- Exercise 8 -----------------------------------------
 
 class Num a => Differentiable a where
     deriv  :: a -> a
     nderiv :: Int -> a -> a
-    nderiv = undefined
+    nderiv n = foldl (.) id (replicate n deriv)
 
 -- Exercise 9 -----------------------------------------
 
 instance Num a => Differentiable (Poly a) where
-    deriv = undefined
-
+    deriv (P y) = P $ case y of
+      [] -> []
+      _:zs -> map (\(degree, coeff) -> fromInteger degree * coeff) $ zip [1..] zs
