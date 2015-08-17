e65127f89417334bdb40da7c2017a5be2824fbf3
Chen Shen <chenshen@fb.com>
2015-08-17 00:49:32 -0700

    fix bug

--- HW05.hs
+++ HW05.hs
@@ -2,13 +2,14 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module HW05 where
 
+import Control.Applicative
 import Data.ByteString.Lazy (ByteString)
 import Data.Map.Strict (Map)
 import System.Environment (getArgs)
 
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Map.Strict as Map
-imoprt qualified Data.Bits as Bits
+import qualified Data.Bits as Bits
 
 import Parser
 
@@ -17,23 +18,23 @@ import Parser
 getSecret :: FilePath -> FilePath -> IO ByteString
 getSecret originalFile encryptedFile = do
   originalContent <- BS.readFile originalFile
-  encryptedContent <- Bs.readFile encryptedFile
-  return filter (\word -> word /= (fromIntegral 0))) $ map (\(word0, word1) -> Bits.xor word0 word) $ zip originalContent encryptedContent
+  encryptedContent <- BS.readFile encryptedFile
+  return $ BS.pack $ filter (\word -> word /= (fromIntegral 0)) $ map (\(word0, word1) -> Bits.xor word0 word1) $ BS.zip originalContent encryptedContent
 
 -- Exercise 2 -----------------------------------------
 
 decryptWithKey :: ByteString -> FilePath -> IO ()
 decryptWithKey key encryptedFile = do
   encryptedContent <- BS.readFile $ encryptedFile ++ ".enc"
-  let decypher content = map (\(word0, word1) -> Bits.xor word0 word1) $ zip content $ cycle key
-  BS.writeFile $ decypher encryptedFile encryptedFile
+  let decypher content = BS.pack $ map (\(word0, word1) -> Bits.xor word0 word1) $ BS.zip content $ BS.cycle key
+  BS.writeFile encryptedFile $ decypher encryptedContent
 
 -- Exercise 3 -----------------------------------------
 
 parseFile :: FromJSON a => FilePath -> IO (Maybe a)
 parseFile jsonFilePath = do
   fileContent <- BS.readFile jsonFilePath
-  return decode fileContent
+  return $ decode fileContent
 
 -- Exercise 4 -----------------------------------------
 
@@ -41,24 +42,26 @@ getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
 getBadTs victimsListFile transactionFile = do
   maybeVictims <- parseFile victimsListFile :: IO (Maybe [TId])
   maybeTransactions <- parseFile transactionFile :: IO (Maybe [Transaction])
-  return getBadTs' maybeVictims maybeTransactions
+  return $ getBadTs' maybeVictims maybeTransactions
   where
-    getBadTs'
-      | Nothing _ = Nothing
-      | (Just victims) maybeTransactions = map (\t -> any (\victim -> victim == tid t) victims) <$> maybeTransactions
+    getBadTs' maybeV maybeT = case maybeV of
+      Nothing -> Nothing
+      (Just victims) -> (filter (\t -> any (\victim -> victim == tid t) victims)) <$> maybeT
 
 -- Exercise 5 -----------------------------------------
 
 getFlow :: [Transaction] -> Map String Integer
-getFlow = foldl addTransaction emty
+getFlow = foldl addTransaction Map.empty
   where
-    addTransaction' flowMap name value = let balance = lookup name flowMap
+    addTransaction' flowMap name value =
+      let balance = Map.lookup name flowMap
       in case balance of
-        Nothing -> insert name value flowMap
-        Just b -> insert name (value + b) flowMap
+        Nothing -> Map.insert name value flowMap
+        Just b -> Map.insert name (value + b) flowMap
     addTransaction flowMap transaction =
-      addTransaction flowMap (from transaction) ((-1) * (amount transaction))
-      addTransaction flowMap (to transaction) (amount transaction)
+      addTransaction' flowMap (from transaction) ((-1) * (amount transaction))
+      addTransaction' flowMap (to transaction) (amount transaction)
+
 -- Exercise 6 -----------------------------------------
 
 getCriminal :: Map String Integer -> String
